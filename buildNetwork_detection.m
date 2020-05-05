388dee0fb5e55654786aacd86a518b24d4d7878d
Joel Deutsch <jdeutsch2013@gmail.com>
2020-05-04 20:35:03 -0500

    Almost have ready to run in Matlab 2018a

--- buildNetwork_detection.m
+++ buildNetwork_detection.m
@@ -1,6 +1,6 @@
 function net = buildNetwork_detection(dsTrain,inputSize,...
     filterSize,numFilters,maxPool,dropout)
-
+%{
 lgraph = layerGraph();
 
 tempLayers = [
@@ -43,13 +43,34 @@ lgraph = connectLayers(lgraph,"relu_6","addition/in2");
 
 clear tempLayers;
 
+%}
+
+layers = [
+    imageInputLayer(inputSize,"Name","imageinput_1")
+    convolution2dLayer([filterSize filterSize],numFilters,"Name","conv_1","Padding","same")
+    reluLayer("Name","relu_1")
+    maxPooling2dLayer([maxPool maxPool],"Name","maxpool_1","Padding","same")
+    dropoutLayer(dropout,"Name","dropout_1")
+    convolution2dLayer([filterSize filterSize],numFilters,"Name","conv_2","Padding","same")
+    reluLayer("Name","relu_2")
+    maxPooling2dLayer([maxPool maxPool],"Name","maxpool_2","Padding","same")
+    dropoutLayer(dropout,"Name","dropout_3")
+    convolution2dLayer([filterSize filterSize],numFilters,"Name","conv_5","Padding","same")
+    reluLayer("Name","relu_5")
+    fullyConnectedLayer(2,"Name","fc")
+    softmaxLayer("Name","softmax")
+    classificationLayer("Name","classoutput")];
+
 options = trainingOptions('adam',...
-    'ExecutionEnvironment', 'cpu',...
-    'MaxEpochs', 350,...
+    'ExecutionEnvironment', 'parallel',...
+    'MaxEpochs', 250,...
+    'MiniBatchSize',64,...
     'GradientThreshold', 2,...
-    'Shuffle', 'never',...
-    'Verbose',0);
+    'Shuffle', 'every-epoch',...
+    'Verbose',0,...
+    'Plots','training-progress');
 
+net = trainNetwork(dsTrain, layers, options);
 
-net = trainNetwork(dsTrain, lgraph, options);
+%net = trainNetwork(dsTrain, lgraph, options);
 end
--- evaluateResults.m
+++ evaluateResults.m
@@ -1,13 +1,14 @@
-function evaluateResults(dsTest,pred,scenario,path)
-
+function evaluateResults(testLabels,pred,scenario,path)
+%{
 targetLabels = strings(1,length(dsTest.Files));
 i=1;
 while hasdata(dsTest)
     targetLabels(i) = string(read(dsTest));
     i = i+1;
 end
+%}
 
-target = [(strcmp(targetLabels,"ictal"));(strcmp(targetLabels,"interictal"))];
+target = [(strcmp(testLabels,"ictal"));(strcmp(testLabels,"interictal"))];
 output = [(pred=="ictal")'; (pred=="interictal")'];
 
 h=figure();
@@ -15,14 +16,14 @@ plotroc(target,output);
 axesUserData=get(gca,'userdata');
 legend(axesUserData.lines,'ictal','interictal');
 title("Scenario " + scenario);
-saveas(h,strcat(path,num2str(scenario)),'fig');
+saveas(h,strcat(path,"\ROC",num2str(scenario)),'fig');
 close(h);
 
 h=figure();
 [c,cm,ind,per] = confusion(int8(target),int8(output));
 plotconfusion(int8(target),int8(output));
 title("Scenario " + scenario);
-saveas(h,strcat(path,num2str(scenario)),'fig');
+saveas(h,strcat(path,"\Confusion",num2str(scenario)),'fig');
 close(h);
 
 end
